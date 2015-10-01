9532369cf69c53f0382eaf2070c7f73c5a10bac7
somename <someemail>
2015-10-01 12:38:06 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -8,7 +8,8 @@ public class TestSudokuVerifier {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		String candidateSolution = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+		SudokuVerifier.this.verify(candidateSolution);
 	}
 
 }
