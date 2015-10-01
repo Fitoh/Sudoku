1ce02922bfe6b4c9e9f41be5cfecfaa7b9ed5ce7
somename <someemail>
2015-10-01 12:55:51 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -10,7 +10,7 @@ public class SudokuVerifier {
 	static boolean hasRepeatingValuesInColumn = false;
 	public static int verify(String candidateSolution) {
 		
-		if (candidateSolution.equals(2)){
+		if (candidateSolution.equals("2")){
 			System.out.println("TROLOLOLOLOO");
 		}
 		
